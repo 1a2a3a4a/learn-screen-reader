dc0bae641b9aeebdb19fa759e4532bf616aadf58
1a2a3a4a <tonywang3334@gmail.com>
2021-03-27 12:44:07 +0100

    completed sttep gitthub fix

--- src/steps/completed/Completed.svelte
+++ src/steps/completed/Completed.svelte
@@ -1,30 +1,33 @@
-<script> 
-    import { useFocus } from "svelte-navigator";
-    const registerFocus = useFocus();
+<script>
+  import { useFocus } from "svelte-navigator";
+  const registerFocus = useFocus();
 </script>
-
-<style>
-    /* your styles go here */
-</style>
 
 <h1 use:registerFocus>All steps completed</h1>
 <h2>NVDA references</h2>
 <ul>
-    <li>
-        <a href="https://webaim.org/articles/nvda/">Webaim</a>
-    </li>
-    <li>
-        <a href="https://dequeuniversity.com/screenreaders/nvda-keyboard-shortcuts">Deque University</a>
-    </li>
+  <li>
+    <a href="https://webaim.org/articles/nvda/">Webaim</a>
+  </li>
+  <li>
+    <a href="https://dequeuniversity.com/screenreaders/nvda-keyboard-shortcuts"
+      >Deque University</a
+    >
+  </li>
 </ul>
 <h2>Voiceover references</h2>
 <ul>
-  
-    <li>
-        <a href="https://webaim.org/articles/voiceover/">Webaim</a>
-    </li>
+  <li>
+    <a href="https://webaim.org/articles/voiceover/">Webaim</a>
+  </li>
 </ul>
 <div class="mt-10 flex flex-col">
-    <a href="https://github.com/1a2a3a4a/learn-screen-reader">Source code on github</a>
-    <a href="mail:to">Mail feedback or contact me</a>
+  <a target="_blank" href="http://github.com/1a2a3a4a/learn-screen-reader"
+    >Source code on github <span class="sr-only">, Opens in new tab</span></a
+  >
+  <a href="mail:to">Mail feedback or contact me</a>
 </div>
+
+<style>
+  /* your styles go here */
+</style>
