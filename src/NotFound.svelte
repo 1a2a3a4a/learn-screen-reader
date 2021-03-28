51942b68bd7c8d7b22504cf627eac345681bf36c
1a2a3a4a <tonywang3334@gmail.com>
2021-03-28 14:30:58 +0200

    import registerfocus to not found

--- src/NotFound.svelte
+++ src/NotFound.svelte
@@ -1,13 +1,14 @@
 <script>
-
-import { Link } from "svelte-navigator";
+  import { Link } from "svelte-navigator";
+  import { useFocus } from "svelte-navigator";
+  const registerFocus = useFocus();
 </script>
-
-<style>
-    /* your styles go here */
-</style>
 
 <!-- markup (zero or more items) goes here -->
 <h1 use:registerFocus>404 page</h1>
 <p>This is the 404 page</p>
 <Link to="/">Go home</Link>
+
+<style>
+  /* your styles go here */
+</style>
