1300ea969c22392f3a0b8d07dc73a997d03c3e2b
1a2a3a4a <tonywang3334@gmail.com>
2021-05-22 10:26:17 +0200

    Spelling error

--- src/steps/startScreenReader/VoiceOver.svelte
+++ src/steps/startScreenReader/VoiceOver.svelte
@@ -9,4 +9,4 @@
 
 <p>The VoiceOver Activation keys (called <strong>VO</strong> keys) are <kbd>control</kbd> + <kbd>option</kbd> or <kbd>CAPS</kbd>and will be referenced simply as <kbd>VO</kbd>. For example, <kbd>VO</kbd> + <kbd>H</kbd> (or <kbd>control</kbd> + <kbd>option</kbd> + <kbd>H</kbd>) will open the VoiceOver Help menu.</p>
 
-<p>Voiceover works best with Safari and also make sure that if you are using safari then enable accessibility by going to Advanced then Accessibility and presse Tab to highlight each item on webpage.</p>
+<p>Voiceover works best with Safari and also make sure that if you are using safari then enable accessibility by going to Advanced then Accessibility and press Tab to highlight each item on webpage.</p>
